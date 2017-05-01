79d5648e58ae1d0e87349f6a89febfdc157a56bf
Rajesh Kumar <rajeshkumar@xebia.com>
2017-05-01 19:00:55 +0530

    Fork Join Pool

---
+++ src/main/java/com/example/practice/forkjoin/RecursiveActionExample.java
@@ -0,0 +1,51 @@
+package com.example.practice.forkjoin;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by rajeshkumar on 01/05/17.
+ */
+public class RecursiveActionExample {
+
+    public static void main(String[] args) throws InterruptedException {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+
+        forkJoinPool.invoke(new RecursiveActionImpl(getWorkList()));
+        forkJoinPool.shutdown();
+        try {
+            forkJoinPool.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List getWorkList() {
+        List workList = new ArrayList(100);
+        for (int counter = 0; counter < 100; counter++) {
+            workList.add("Task: " + counter);
+        }
+        return workList;
+    }
+}
+
+class RecursiveActionImpl extends RecursiveAction {
+    List workList;
+
+    public RecursiveActionImpl(final List workList) {
+        this.workList = workList;
+    }
+
+    @Override
+    protected void compute() {
+        if (workList.size() > 1) {
+            new RecursiveActionImpl(workList.subList(0, workList.size() / 2)).fork();
+            new RecursiveActionImpl(workList.subList((workList.size() / 2), workList.size())).fork();
+        } else {
+            System.out.println("workList: " + workList.get(0));
+        }
+    }
+}
